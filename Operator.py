bdff0269ad71b4987203c3e1fb769b8aa78b8c82
todbott <todd.g@listenfield.com>
2023-02-07 18:02:10 +0900

    Added a silent, non-internet-dependent class in the 'Operator.py' file

--- Operator.py
+++ Operator.py
@@ -32,7 +32,7 @@ class SensorController:
 
         # Here are the buzzers, LEDs and button
         self.monotone = Pin(32, Pin.OUT)
-        self.polytone = PWM(Pin(33), freq=500, duty=0)
+        self.polytone = PWM(Pin(33), freq=200, duty=0)
         self.red = PWM(Pin(16), freq=5000, duty=500)
         self.yellow = PWM(Pin(18), freq=5000, duty=0)
         self.green = PWM(Pin(19), freq=5000, duty=0)
@@ -366,4 +366,274 @@ class SensorController:
 
 
 
+class NightSensorController:
+
+    def __init__(self, minutes, first_warning, second_warning) -> None:
+
+        # Initialize the sensor
+        self.i2c = I2C(scl=Pin(22), sda=Pin(21))  
+         
+        # Make a variable to hold sensor readiings
+        self.mpu= mpu6050.accel(self.i2c)
+
+        # Variables that were passed in
+        self.minutes = minutes
+        self.first_warning = first_warning
+        self.second_warning = second_warning
+
+        # Here are the buzzers, LEDs and button
+        self.monotone = Pin(32, Pin.OUT)
+        self.polytone = PWM(Pin(33), freq=300, duty=0)
+        self.red = PWM(Pin(16), freq=5000, duty=500)
+        self.yellow = PWM(Pin(18), freq=5000, duty=0)
+        self.green = PWM(Pin(19), freq=5000, duty=0)
+        self.button = machine.Pin(17, machine.Pin.IN, machine.Pin.PULL_UP)
+
+        # Here are dictionaries to hold the readings and stuff
+        self.no_movement_seconds = 0
+        self.movement_log = {
+            'seconds_until_movement': []
+        }
+        self.readings = {
+            
+            'GyX': [],
+            'GyY': [],
+            'GyZ': [],
+
+            'AcX': [],
+            'AcY': [],
+            'AcZ': []
+        }
+
+        self.finalReadings = {
+            
+            'GyX': [],
+            'GyY': [],
+            'GyZ': [],
+
+            'AcX': [],
+            'AcY': [],
+            'AcZ': []
+        }
+
+        self.thresholds = {
+
+            'GyX': 10,
+            'GyY': 10,
+            'GyZ': 10,
+
+            'AcX': 10,
+            'AcY': 10,
+            'AcZ': 10
+        }
+
+
+    def __averageList(self, listToAverage):
     
+        av = 0
+        for v in listToAverage:
+            av+=v
+        return av/len(listToAverage)
+
+    def __calculateSensorError(self):
+
+
+        calculated = False
+        while not calculated:
+
+            lists_remain = False
+
+            mpuv = self.mpu.get_values()
+            self.yellow.duty(50)
+            print("Calculating the error rates of each sensor")
+                    
+            for k in self.readings.keys():
+                if isinstance(self.finalReadings[k], list):
+                    lists_remain = True
+                    if len(self.readings[k]) > 5:
+                        self.finalReadings[k].append(self.__averageList(self.readings[k]))
+                        self.readings[k].clear()
+                        if len(self.finalReadings[k]) > 5:
+                            self.finalReadings[k] = self.__averageList(self.finalReadings[k])
+                    self.readings[k].append(mpuv[k])
+
+            time.sleep(0.05)
+            self.yellow.duty(0)
+            time.sleep(0.05)
+
+            if not lists_remain:
+                calculated = True
+        return
+
+    def __calibrateSensor(self):
+        seconds_without_error = 0
+        adjustment = False
+        calibrated = False
+        while not calibrated:
+
+            mpuv = self.mpu.get_values()
+
+            for k in self.readings.keys():
+
+                self.yellow.duty(500)
+                self.readings[k] = mpuv[k]
+                
+                if math.fabs(self.readings[k]-self.finalReadings[k]) > self.thresholds[k]:
+                    self.thresholds[k] += math.fabs(self.readings[k]-self.finalReadings[k])
+                    adjustment = True
+
+            if adjustment:
+                adjustment = False
+                seconds_without_error = 0
+            else:
+                seconds_without_error += 1
+                print(f'{seconds_without_error} seconds with no error')
+            if seconds_without_error > self.minutes * 60:
+                calibrated = True
+            self.yellow.duty(0)
+            time.sleep(1)
+            
+        for k in self.readings.keys():
+            self.readings[k] = []
+            self.thresholds[k] += 100
+
+        with open("calibration_values.txt", "w") as v:
+            ujson.dump(self.thresholds, v)
+
+    def __recalibrateOnStartupMaybe(self):
+        # Check if the button is pressed just after startup.  If it is, that means that the user wants to
+        # recalibrate the sensors, so erase the calibration_values.txt file
+        if self.button.value():
+            with open("calibration_values.txt", "w") as v:
+                v.write('')
+                v.close()
+                for x in range(500, 0, -50):
+                    self.green.duty(x)
+                    time.sleep(0.25)
+            self.green.duty(0)
+
+    def __checkForMovementAndMakeReplies(self, movement_this_time):
+        if not movement_this_time:
+            self.no_movement_seconds += 3
+        else:
+            self.movement_log['seconds_until_movement'].append(self.no_movement_seconds)
+            print(f"moving at {self.no_movement_seconds}")
+            self.no_movement_seconds = 0
+            
+        #print(self.no_movement_seconds)
+
+        if self.no_movement_seconds > self.second_warning:
+            self.red.duty(500)
+            self.polytone.duty(6)  
+            #self.monotone.value(1)     
+        elif self.no_movement_seconds > self.first_warning:
+            self.red.duty(250)
+            self.polytone.duty(3)
+        else:
+            self.red.duty(0)
+            self.polytone.duty(0)
+            #self.monotone.value(0)
+
+    def main(self):
+
+        self.polytone.duty(6)
+        time.sleep(.5)
+        self.polytone.duty(0)
+
+        self.yellow.duty(0)
+        self.red.duty(0)
+
+        # Check if the button is pressed.  If so, erase the contents of calibration_values.txt
+        self.__recalibrateOnStartupMaybe()
+
+        # If calibration_values.txt is empty, re-calibrate the sensors
+        vals = open("calibration_values.txt", "r").read()
+        if len(vals) == 0:
+            self.__calculateSensorError()
+            self.__calibrateSensor()
+        else:
+            with open("calibration_values.txt", "r") as v:
+                self.thresholds = ujson.load(v)
+
+        # Start looping, sending sensor updates to the site
+        while True:
+            
+            movement_this_time = False
+            
+            # The mpu6050.py file occasionally returns an error temporarily,
+            # so here we try to get the values, and if there's an error, we just fill
+            # the mpuv dictionary with the most recent values in the readings dictionary.
+            # If there was an error, we'll just activate the buzzer for a split second (for debugging purposes)
+            try:
+                mpuv = self.mpu.get_values()
+            except:
+                
+                self.yellow.duty(250)
+                mpuv = {}
+                for k in self.readings.keys():
+                    mpuv[k] = self.readings[k][0]
+                time.sleep(0.25)
+                self.yellow.duty(0)
+                print("sensor momentarily disconnected")
+                
+                            
+            for k in self.readings.keys():
+
+                self.readings[k].append(mpuv[k])
+                
+                # If there are 2 readings, compare the difference in those readings against the threshold value for that sensor
+                if len(self.readings[k]) == 3:
+
+                    if self.__averageList(self.readings[k]) == 0:
+                        
+                        # Initialize the sensor
+                        self.i2c = I2C(scl=Pin(22), sda=Pin(21))  
+                        
+                        # Make a variable to hold sensor readiings
+                        self.mpu= mpu6050.accel(self.i2c)
+        
+                    if math.fabs(self.readings[k][1]-self.readings[k][0]) > self.thresholds[k]:
+                        movement_this_time = True
+                    self.readings[k] = self.readings[k][1:]
+                    
+            if movement_this_time:
+               self.green.duty(100)
+            time.sleep(1)
+            self.green.duty(0)
+
+            # Make a | delimited response to send to the frontend
+            self.__checkForMovementAndMakeReplies(movement_this_time)
+
+            time.sleep(1)
+            
+            # If the button was pressed, the user wants to stop the monitoring
+            if self.button.value():
+                self.polytone.duty(0)
+                self.monotone.value(0)
+                self.green.duty(0)
+                break
+
+        # Here, we know the button has been pressed, or an error has occcured
+        print('Problem here, or sensor has been stopped!')
+
+        with open("log.txt", "w") as v:
+            ujson.dump(self.movement_log, v)
+        
+        # Turn on the red and yellow LEDs!
+        self.green.duty(0)
+        self.yellow.duty(500)
+        self.red.duty(500)
+
+        time.sleep(3)
+
+        # Keep the buzzer and LEDs on until the button is pressed, then break
+        while True:
+            if self.button.value():
+                # As a final step, the system turns off all lights and buzzers, as a kind of cleanup operation
+                self.polytone.duty(0)
+                self.monotone.value(0)
+                self.green.duty(0)
+                self.red.duty(0)
+                self.yellow.duty(0)
+
+                break
